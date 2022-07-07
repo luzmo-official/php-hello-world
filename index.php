f1a11df086e9ee5109f88018df4424de0492f220
Paresh Chouhan <paresh@cumul.io>
2022-07-07 14:26:20 +0530

    Updated .env.example

--- index.php
+++ index.php
@@ -6,19 +6,25 @@ header('Content-Type: application/json');
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
-$client = Cumulio::initialize($_ENV['CUMUL_KEY'], $_ENV['CUMUL_TOKEN'], 'https://api.cumul.io');
+$client = Cumulio::initialize($_ENV['CUMUL_KEY'], $_ENV['CUMUL_TOKEN'], $_ENV['API_URL']);
 
 $authorization = $client->create('authorization', array(
   'integration_id' => $_ENV['INTEGRATION_ID'],
   'type' => 'sso',
   'expiry' => '24 hours',
   'inactivity_interval' => '10 minutes',
-  'username' => '< A unique identifier for your user >',
+  'username' => $_ENV['USER_USERNAME'],
   'name' => $_ENV['USER_NAME'],
   'email' => $_ENV['USER_EMAIL'],
-  'suborganization' => '< user suborganization >',
+  'suborganization' => $_ENV['USER_SUBORGANIZATION'],
   'role' => 'viewer'
 ));
 
-echo json_encode($_ENV, JSON_PRETTY_PRINT)
+$authResponse = array(
+  'status' => 'success',
+  'key' => $authorization['id'],
+  'token' => $authorization['token']
+);
+
+echo json_encode($authResponse, JSON_PRETTY_PRINT)
 ?>
